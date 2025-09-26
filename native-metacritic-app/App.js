7077e29c04e38400b22b87467393c81b90f63975
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-09-26 08:50:00 +0200

    feat: practice TouchableHighlight and Pressable components with button color change on press

--- native-metacritic-app/App.js
+++ native-metacritic-app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight } from 'react-native';
 
 const icon = require('./assets/icon.png');
 
@@ -7,13 +7,41 @@ const icon = require('./assets/icon.png');
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image blurRadius={2} source= {icon} style={{ width: 100, height:100}} resizeMode='contain'/>
-      <Image source={{ uri: "https://metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg"}} style={{ width: 215, height: 294}}/>
+      <Image 
+        blurRadius={2} 
+        source={icon} 
+        style={{ width: 100, height: 100 }} 
+        resizeMode="contain" 
+      />
+
+      <Image 
+        source={{ uri: "https://metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg" }} 
+        style={{ width: 215, height: 294 }} 
+      />
+
       <Text>Tenemos aquí la app</Text>
-      <StatusBar style="dark" />
-    </View>
-  );
-}
+
+      <Button title="pulsa aquí" onPress={() => alert('hola')} />
+
+<TouchableHighlight 
+  underlayColor="#09f" 
+  onPress={() => alert('hola')}
+  style={{ borderRadius: 100 }}
+>
+  <View 
+    style={{ 
+      width: 100, 
+      height: 100, 
+      backgroundColor: 'red', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      borderRadius: 100 
+    }}
+  >
+    <Text style={{ color: 'white' }}>Pulsa aqui</Text>
+  </View>
+</TouchableHighlight>
+</View> )}
 
 const styles = StyleSheet.create({
   container: {
